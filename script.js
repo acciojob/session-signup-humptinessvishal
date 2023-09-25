4128aeb509f876736ad54f7734c615be31966fae
Vishal Kumar <136982736+humptinessvishal@users.noreply.github.com>
2023-09-25 10:58:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -9,6 +9,13 @@ form.addEventListner("submit",(event)=>{
 	const password = document.getElementsById("password").value;
 	const confirmpassword = document.getElementsById("confirm-password").value;
 	if(password === confirmpassword){
+		const testUser = {name};
+		const useremail = {email};
+		const userpassword = {password};
+        localStorage.setItem("name", JSON.stringify(username));
+        localStorage.setItem("email", JSON.stringify(useremail));
+        localStorage.setItem("password", JSON.stringify(userpassword));
+
 		sessionStorage.setItem("name", name);
         sessionStorage.setItem("email", email);
         sessionStorage.setItem("password", password);
